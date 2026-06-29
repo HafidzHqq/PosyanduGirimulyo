8d3331854f09a941ee92dc9c8e41eae856258777
HafidzHaqiqi <gegefans0@gmail.com>
2026-06-29 21:20:52 +0700

    fix: copy static assets to public/_next/static for litespeed

--- copy-assets.mjs
+++ copy-assets.mjs
@@ -14,8 +14,11 @@ function copyRecursiveSync(src, dest) {
   }
 }
 
-console.log("Menyalin folder .next/static ke dalam .next/standalone/.next/static...");
+console.log("Menyalin folder .next/static ke dalam .next/standalone/.next/static (Untuk Node.js)...");
 copyRecursiveSync(path.join('.next', 'static'), path.join('.next', 'standalone', '.next', 'static'));
+
+console.log("Menyalin folder .next/static ke dalam .next/standalone/public/_next/static (Untuk Hostinger LiteSpeed)...");
+copyRecursiveSync(path.join('.next', 'static'), path.join('.next', 'standalone', 'public', '_next', 'static'));
 
 console.log("Menyalin folder public ke dalam .next/standalone/public...");
 copyRecursiveSync('public', path.join('.next', 'standalone', 'public'));
